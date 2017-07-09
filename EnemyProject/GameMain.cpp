5953dfd8fe753d3ca78e0af61f43b9264c4beaaa
kimi28 <kimhaidong28@gmail.com>
2017-07-09 22:48:56 +0900

    GameMain 수정

--- EnemyProject/GameMain.cpp
+++ EnemyProject/GameMain.cpp
@@ -18,25 +18,25 @@ GameMain::~GameMain()
 
 void GameMain::Initialize()
 {
+	BulletManager::GetInstance()->SetDevice(device);
 	SoundManager::GetInstance()->AddSound("배경", "Sounds/Airport.mp3", true, true);
 	SoundManager::GetInstance()->Play("배경");
 
-	animation = new Animation(device, L"Textures/cat.png", { 4,2 }, { 150, 100 });
-	animation->Initialize();
+	//animation = new Animation(device, L"Textures/cat.png", { 4,2 }, { 150, 100 });
+	//animation->Initialize();
 
-	frame = { 0,0 };
+	//frame = { 0,0 };
 
-	time = timeGetTime();
-	//BulletManager::GetInstance()->SetDevice(device);
+	//time = timeGetTime();
 
-	//background = new Background(device);
-	//background->Initialize();
+	background = new Background(device);
+	background->Initialize();
 
-	//player = new Player(device);
-	//player->Initialize();
+	player = new Player(device);
+	player->Initialize();
 
-	//enemy = new Enemy(device, D3DXVECTOR2(900, 350));
-	//enemy->Initialize();
+	enemy = new Enemy(device, D3DXVECTOR2(900, 350));
+	enemy->Initialize();
 
 	//player->EnemyMemoryLink(enemy);
 	//enemy->PlayerMemoryLink(player);
@@ -45,45 +45,45 @@ void GameMain::Initialize()
 void GameMain::Destroy()
 {
 
-	SAFE_DELETE(animation);
-	//BulletManager::DeleteInstance();
+	//SAFE_DELETE(animation);
+	BulletManager::DeleteInstance();
 
-	//background->Destroy();
-	//SAFE_DELETE(background);
+	background->Destroy();
+	SAFE_DELETE(background);
 
-	//player->Destroy();
-	//SAFE_DELETE(player);
+	player->Destroy();
+	SAFE_DELETE(player);
 
-	//enemy->Destroy();
-	//SAFE_DELETE(enemy);
+	enemy->Destroy();
+	SAFE_DELETE(enemy);
 }
 
 void GameMain::Update()
 {
-	DWORD currentTime = timeGetTime();
-	if (currentTime - time > 100) {
-		animation->Update(frame);
-		frame.x++;
-		if (frame.x == 3) {
-			frame.x = 0;
-			if (frame.y == 0)
-				frame.y++;
-			else
-				frame.y--;
-		}
-		time = timeGetTime();
-	}
-	//background->Update();
-	//BulletManager::GetInstance()->Update();
-	//player->Update();
-	//enemy->Update();
+	//DWORD currentTime = timeGetTime();
+	//if (currentTime - time > 100) {
+	//	animation->Update(frame);
+	//	frame.x++;
+	//	if (frame.x == 3) {
+	//		frame.x = 0;
+	//		if (frame.y == 0)
+	//			frame.y++;
+	//		else
+	//			frame.y--;
+	//	}
+	//	time = timeGetTime();
+	//}
+	background->Update();
+	BulletManager::GetInstance()->Update();
+	player->Update();
+	enemy->Update();
 }
 
 void GameMain::Render()
 {
-	animation->Render();
-	//background->Render();
-	//BulletManager::GetInstance()->Render();
-	//player->Render();
-	//enemy->Render();
+	//animation->Render();
+	background->Render();
+	BulletManager::GetInstance()->Render();
+	player->Render();
+	enemy->Render();
 }
