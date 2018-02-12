e160d241f46c9f3c075cd9e234bbae0131bde5af
kimi28 <kimhaidong28@gmail.com>
2018-02-12 09:39:21 +0900

    Day3_1 Shader파일 추가
    Shader파일 아래 생성된 내용:
    1. ColorPixel.hlsl
    2. ColorVertex.hlsl
    * hlsl파일 속성에서 hlsl컴파일을 제외를 설정하기.

---
+++ DirectX_3D/DxWindows/DxWindows/Shader/ColorPixel.hlsl
@@ -0,0 +1,12 @@
+
+
+struct PixelInput
+{
+	float4 position : SV_POSITION;
+	float4 color :	COLOR0;
+};
+
+float4 ColorPixelShader(PixelInput input) : SV_TARGET
+{
+	return input.color;
+}
---
+++ DirectX_3D/DxWindows/DxWindows/Shader/ColorVertex.hlsl
@@ -0,0 +1,33 @@
+
+
+cbuffer MatrixBuffer
+{
+	matrix world;
+	matrix view;
+	matrix projection;
+};
+
+struct VertexInput
+{
+	float4 position : POSITION0;
+	float4 color :	COLOR0;
+};
+
+struct PixelInput
+{
+	float4 position : SV_POSITION;
+	float4 color : COLOR0;
+};
+
+PixelInput ColorVertexShader(VertexInput input)
+{
+	PixelInput output;
+
+	output.position = mul(input.position, world);
+	output.position = mul(output.position, view);
+	output.position = mul(output.position, projection);
+
+	output.color = intput.color;
+
+	return output;
+}
