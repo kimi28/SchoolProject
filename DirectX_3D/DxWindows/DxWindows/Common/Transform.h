acb27acbf210eb5a48868bd41b814dea05ab1a7c
kimi28 <kimhaidong28@gmail.com>
2018-02-18 11:32:44 +0900

    Day4_3 Camera, Transform Class 추가

---
+++ DirectX_3D/DxWindows/DxWindows/Common/Transform.h
@@ -0,0 +1,64 @@
+#pragma once
+#define	RESET_POSITION	1
+#define	RESET_RPTATION	2
+#define	RESET_SCALE		4
+
+#define	AXIS_X	0
+#define	AXIS_Y	1
+#define	AXIS_Z	2
+
+#define	SHAKE_X	1
+#define	SHAKE_Y	2
+#define	SHAKE_Z	4
+
+class Transform
+{
+protected:
+	union
+	{
+		struct
+		{
+			D3DXVECTOR3	axis[3];
+		};
+		struct
+		{
+			D3DXVECTOR3	right;
+			D3DXVECTOR3	up;
+			D3DXVECTOR3	forward;
+		};
+	};
+
+	D3DXVECTOR3	position;
+	D3DXVECTOR3	scale;
+
+	D3DXMATRIX	matFinal;
+
+	bool	bAutoUpdate;
+
+public:
+	Transform();
+	~Transform();
+
+
+	void SetAutoUpdate(bool bAuto)
+	{
+		this->bAutoUpdate = bAuto;
+	}
+	
+	void Reset(int resetFlag = -1);
+
+	void SetWorldPosition(float x, float y, float z);
+	void SetWorldPosition(D3DXVECTOR3 pos);
+
+	void SetLocalPosition(float x, float y, float z);
+	void SetLocalPosition(D3DXVECTOR3 pos);
+
+	void MovePositionSelf(float dx, float dy, float dz);
+	void MovePositionSelf(D3DXVECTOR3 delta);
+
+	void MovePositionLocal(float dx, float dy, float dz);
+	void MovePositionLocal(D3DXVECTOR3 delta);
+
+
+};
+
